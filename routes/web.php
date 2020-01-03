70a5bc05e953fc0ea73e509e217ff952c7ad9e86
kylinfish <kylinfish@gmail.com>
2020-01-03 18:00:02 +0800

    [Router] Add customer router

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get('health/check', 'HealthController@check');
+$router->get('/', function () use ($router) {
+    return 'Hello, this is macarame-api service.';
+});
+
+// Customer
+$router->get('customers', [
+    'as' => 'customers.index',
+    'uses' => 'CustomersController@index'
+]);
+$router->get('customers/{customer_id}', [
+    'as' => 'customers.show',
+    'uses' => 'CustomersController@show'
+]);
+$router->post('customer', [
+    'as' => 'customers.store',
+    'uses' => 'CustomersController@store'
+]);
+$router->put('customer/{customer_id}', [
+    'as' => 'customers.update',
+    'uses' => 'CustomersController@update'
+]);
+$router->delete('customer/{customer_id}', [
+    'as' => 'customers.delete',
+    'uses' => 'CustomersController@delete'
+]);
