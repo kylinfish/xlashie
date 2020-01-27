dfb097acca12a7e16d9c3e9ee7c20a952073a6c6
Ray Chang <ray20204@gmail.com>
2020-01-27 13:39:04 +0800

    add facebook socialite config

--- config/services.php
+++ config/services.php
@@ -7,4 +7,9 @@
  */
 
 return [
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_KEY'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect'      => env('FACEBOOK_REDIRECT_URI')
+    ]
 ];
