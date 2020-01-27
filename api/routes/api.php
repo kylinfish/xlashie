e4ed7b3a53491b89423ac27847f5baba802e0ace
Ray Chang <ray20204@gmail.com>
2020-01-27 22:28:20 +0800

    migrate api route rule

--- api/routes/api.php
+++ api/routes/api.php
@@ -16,3 +16,40 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('health/check', 'HealthController@check');
+Route::get('/', function () use ($router) {
+    return 'Hello, this is macarame-api service.';
+});
+Route::prefix('login')->group(function () {
+    Route::get('facebook', [
+        'as' => 'login.facebook',
+        'uses' => 'Auth\LoginController@facebook'
+    ]);
+    Route::get('facebook/callback', [
+        'as' => 'login.facebookcallback',
+        'uses' => 'Auth\LoginController@facebookCallback'
+    ]);
+});
+
+// Customer
+Route::get('customers', [
+    'as' => 'customers.index',
+    'uses' => 'CustomerController@index'
+]);
+Route::get('customers/{cusotmer_uuid}', [
+    'as' => 'customers.show',
+    'uses' => 'CustomerController@show'
+]);
+Route::post('customer', [
+    'as' => 'customers.store',
+    'uses' => 'CustomerController@store'
+]);
+Route::put('customers/{cusotmer_uuid}', [
+    'as' => 'customers.update',
+    'uses' => 'CustomerController@update'
+]);
+Route::delete('customers/{cusotmer_uuid}', [
+    'as' => 'customers.delete',
+    'uses' => 'CustomerController@delete'
+]);
