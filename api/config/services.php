6b39660d24a9a61ad12bbad8ea4db36b33c03c52
Ray Chang <ray20204@gmail.com>
2020-01-27 21:55:05 +0800

    create laravel project

---
+++ api/config/services.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| socialite config
+|--------------------------------------------------------------------------
+ */
+
+return [
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_KEY'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect'      => env('FACEBOOK_REDIRECT_URI')
+    ]
+];
