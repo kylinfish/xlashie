f28b91b207423896a97370099a48714a2670bf0e
Ray Chang <ray20204@gmail.com>
2020-02-17 21:57:10 +0800

    [config] add google auth config

--- api/config/services.php
+++ api/config/services.php
@@ -11,5 +11,10 @@ return [
         'client_id'     => env('FACEBOOK_KEY'),
         'client_secret' => env('FACEBOOK_SECRET'),
         'redirect'      => env('FACEBOOK_REDIRECT_URI')
+    ],
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_REDIRECT_URI')
     ]
 ];
