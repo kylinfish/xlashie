176b6a6017737939f476dd6c6b28c1130f3a59c8
kylinfish <kylinfish@gmail.com>
2020-02-28 16:21:28 +0800

    [api] Add customer@create action

--- api/app/Http/Controllers/CustomerController.php
+++ api/app/Http/Controllers/CustomerController.php
@@ -3,34 +3,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Forms\CustomerForm;
+use App\Services\CustomerService;
 use App\Repositories\CustomerRepository;
-#use App\Forms\VendorForm;
-#use App\Services\Benefit\VendorService;
 use App\Models\User;
 
 class CustomerController extends \App\Http\Controllers\Controller
 {
-    /*
-     * __construct
-     *
-     * @param VendorForm $form
-     * @param VendorService $service
-     * @return void
-     */
-    public function __construct(CustomerRepository $customer_repo)
+    public function __construct(CustomerRepository $customer_repo, CustomerForm $form, CustomerService $service)
     {
         // hardcode should be instead of real query
         $this->user_id = User::find(1)->first()->id;
+
+        $this->form = $form;
+        $this->service = $service;
         $this->repo = $customer_repo;
     }
 
-    /**
-     * index list of customers
-     *
-     * @param Request $request
-     * @param int $vendor_id
-     * @return json
-     */
     public function index(Request $request)
     {
         $customers = $this->repo->getCustomers($this->user_id);
@@ -38,12 +27,6 @@ class CustomerController extends \App\Http\Controllers\Controller
         return response()->json($customers, 200);
     }
 
-    /**
-     * show
-     *
-     * @param Request $request
-     * @return json
-     */
     public function show(Request $request, string $customer_uuid)
     {
         $this->form->validate($request->all());
@@ -53,4 +36,18 @@ class CustomerController extends \App\Http\Controllers\Controller
         return response()->json(['data' => $customer], 200);
     }
 
+    public function store(Request $request)
+    {
+        $params = $request->all();
+
+        $this->form->validate($params);
+
+        $params["user_id"] = $this->user_id;
+
+        $customer = $this->service->createCustomer($params);
+
+        return response()->json([
+            'message' => "客戶: {$customer->name} 新增成功",
+        ], 200);
+    }
 }
