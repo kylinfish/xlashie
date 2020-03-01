31fd03435af3e09bfb27a7269ef3d16fdbdcea99
Ray Chang <ray20204@gmail.com>
2020-03-01 17:09:06 +0800

    [config] add jwt auth config

--- api/config/app.php
+++ api/config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        // Package jwt-auth
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
         /*
          * Package Service Providers...
          */
