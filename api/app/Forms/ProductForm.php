c51c94c684901f15bec9ca963706aacac313a620
kylinfish <kylinfish@gmail.com>
2020-05-01 18:43:56 +0800

    [form] Add products validate rule

--- api/app/Forms/ProductForm.php
+++ api/app/Forms/ProductForm.php
@@ -6,11 +6,19 @@ use App\Forms\BaseForm;
 class ProductForm extends BaseForm
 {
     protected $store = [
+        'name' => 'required|string|max:50',
+        'avatar' => 'required|string|max:255',
+        'cost' => 'required|numeric|min:0|not_in:0',
+        'price' => 'required|numeric|min:0|not_in:0',
+        'status' => 'integer|max:2',
     ];
 
     protected $index = [
+        'from'    => 'numeric',
+        'limit'   => 'numeric',
     ];
 
     protected $show = [
+        'uuid'    => 'required|string|size:18',
     ];
 }
