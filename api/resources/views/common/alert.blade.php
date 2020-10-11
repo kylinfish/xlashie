7b4ab32b1e9511c8dd19d2de2fa5cad9d6c83be1
kylinfish <kylinfish@gmail.com>
2020-10-11 18:02:37 +0800

    新增一般 view 變數 render 到 alert

--- api/resources/views/common/alert.blade.php
+++ api/resources/views/common/alert.blade.php
@@ -6,3 +6,12 @@
     </button>
 </div>
 @endif
+
+@if (!empty($alert))
+<div class="alert alert-{{ $alert }} alert-dismissible fade show" role="alert">
+    <span class="alert-text">{{ $message }}</span>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
