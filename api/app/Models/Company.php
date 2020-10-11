209fb6e2cf80a905d87be20db0978b16532ff742
kylinfish <kylinfish@gmail.com>
2020-10-11 20:12:08 +0800

    change

--- api/app/Models/Company.php
+++ api/app/Models/Company.php
@@ -27,4 +27,9 @@ class Company extends Model
     {
         return $this->hasMany('App\Models\Menu', 'company_id', 'id')->orderBy('id', 'asc');
     }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'id', 'owner_id');
+    }
 }
--- api/resources/views/layouts/foot.blade.php
+++ api/resources/views/layouts/foot.blade.php
@@ -14,24 +14,9 @@
 <script src="{{ asset('assets/vendor/fullcalendar/dist/fullcalendar.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/sweetalert2/dist/sweetalert2.min.js') }}"></script>
 <script>
-var events = [
-    {
-        id: 1,
-        title: 'Call with Dave',
-        start: '2020-08-31',
-        allDay: true,
-        className: 'bg-red',
-        description: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
-    },
-    {
-        id: 2,
-        title: 'Lunch meeting',
-        start: '2020-09-21',
-        allDay: true,
-        className: 'bg-orange',
-        description: 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
-    }
-]
+$(".alert").fadeTo(2000, 500).slideUp(500, function(){
+    $(".alert").slideUp(500);
+});
 </script>
 
 
