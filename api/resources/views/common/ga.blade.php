10b56d76a4642b922d19f995f100889e079bf703
kylinfish <kylinfish@gmail.com>
2021-02-16 12:01:58 +0800

    Enable ga when on prod

--- api/resources/views/common/ga.blade.php
+++ api/resources/views/common/ga.blade.php
@@ -1,9 +1,11 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-C7JVNY33K6"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+  if(window.location.hostname != "localhost") {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-  gtag('config', 'G-C7JVNY33K6');
+    gtag('config', 'G-C7JVNY33K6');
+  }
 </script>
