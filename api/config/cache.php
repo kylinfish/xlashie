b89a61f74d13c4141635f2aca50ad6c86cfe5d46
kylinfish <kylinfish@gmail.com>
2021-03-04 09:49:34 +0800

    Fix cache config

--- api/config/cache.php
+++ api/config/cache.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_DRIVER', 'memcached'),
+    'default' => env('CACHE_DRIVER', 'file'),
 
     /*
     |--------------------------------------------------------------------------
@@ -74,6 +74,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', 'lumen'),
+    'prefix' => env('CACHE_PREFIX', "laravel_cache"),
 
 ];
