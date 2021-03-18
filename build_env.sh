db10ea3f5dd2d77a150cd627d219b56ae3bc4093
kylinfish <kylinfish@gmail.com>
2021-03-18 14:29:59 +0800

    Extract additional config for ci deployment control

--- build_env.sh
+++ build_env.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-cp api/.env.example ./api/.env
+cp api/.env.example .env
 
-sed -i -e "s/APP_DEBUG=true/APP_DEBUG=false/g" ./api/.env
-sed -i -e "s/DB_HOST=127.0.0.1/DB_HOST=192.168.12.129/g" ./api/.env
-sed -i -e "s/DB_DATABASE=macrame/DB_DATABASE=${DB_USERNAME}/g" ./api/.env
-sed -i -e "s/DB_PASSWORD=12345/DB_DATABASE=${DB_PASSWORD}/g" ./api/.env
-sed -i -e "s/http:\/\/localhost/http:\/\/${FQDN}/g" ./api/.env
+sed -i -e "s/APP_DEBUG=true/APP_DEBUG=false/g" .env
+sed -i -e "s/{DB_HOST_IP}/${DB_HOST_IP}/g" .env
+sed -i -e "s/{DB_DATABASE}/${DB_DATABASE}/g" .env
+sed -i -e "s/{DB_USERNAME}/${DB_USERNAME}/g" .env
+sed -i -e "s/{DB_PASSWORD}/${DB_PASSWORD}/g" .env
+sed -i -e "s/{DB_HOST_IP}/${DB_HOST_IP}/g" .env
+sed -i -e "s/{APP_URL}/http:\/\/${FQDN}/g" .env
+
+sed -i -e "s/{G_CLIENT_ID}/${G_CLIENT_ID}/g" .env
+sed -i -e "s/{G_CLIENT_SECRET}/${G_CLIENT_SECRET}/g" .env
+sed -i -e "s/{G_REDIRECT_URI}/http:\/\/${FQDN}\/login\/google\/callback/g" .env
+
+mv .env ./api/.env
