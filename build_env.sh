329f46af0ff1b18e461113865c6071e172fff59b
kylinfish <kylinfish@gmail.com>
2021-03-21 15:45:07 +0800

    Requre argon-dashboard-free via npm

--- build_env.sh
+++ build_env.sh
@@ -8,10 +8,10 @@ sed -i -e "s/{DB_DATABASE}/${DB_DATABASE}/g" .env
 sed -i -e "s/{DB_USERNAME}/${DB_USERNAME}/g" .env
 sed -i -e "s/{DB_PASSWORD}/${DB_PASSWORD}/g" .env
 sed -i -e "s/{DB_HOST_IP}/${DB_HOST_IP}/g" .env
-sed -i -e "s/{APP_URL}/http:\/\/${FQDN}/g" .env
+sed -i -e "s/{APP_URL}/https:\/\/${FQDN}/g" .env
 
 sed -i -e "s/{G_CLIENT_ID}/${G_CLIENT_ID}/g" .env
 sed -i -e "s/{G_CLIENT_SECRET}/${G_CLIENT_SECRET}/g" .env
-sed -i -e "s/{G_REDIRECT_URI}/http:\/\/${FQDN}\/login\/google\/callback/g" .env
+sed -i -e "s/{G_REDIRECT_URI}/https:\/\/${FQDN}\/login\/google\/callback/g" .env
 
 mv .env ./api/.env
