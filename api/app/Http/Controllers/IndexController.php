8254bdb49196a4e250239a608df92ac1c0ab0b7d
kylinfish <kylinfish@gmail.com>
2021-03-26 16:50:43 +0800

    首頁新增指引提示，以及數量

--- api/app/Http/Controllers/IndexController.php
+++ api/app/Http/Controllers/IndexController.php
@@ -10,12 +10,16 @@ class IndexController extends \App\Http\Controllers\Controller
     public function index(Request $request)
     {
         $customer_count = my_customer()->count();
+        $menu_count = my_comp()->menu()->count();
+        $ticket_count = my_comp()->ticket()->count();
 
         $recently_tickets = [];
         $recently_tickets = my_comp()->ticket()->with("customer")
             ->orderBy("created_at", "DESC")->limit(5)->get();
 
-        return view("welcome", compact("customer_count", "recently_tickets"));
+
+
+        return view("welcome", compact("customer_count", "menu_count", "ticket_count", "recently_tickets"));
     }
 
     public function landing()
--- api/resources/views/welcome.blade.php
+++ api/resources/views/welcome.blade.php
@@ -16,14 +16,14 @@
                     <div class="col">
                         <a class="card shadow border-0" href="/menus/">
                             <div class="card-body text-center">
-                                <h4 class="h2 text-primary text-uppercase"><i class="fas fa-cube"></i> 營業項目</h4>
+                                <h4 class="h2 text-primary text-uppercase"><i class="fas fa-cube"></i> 營業項目 <small>({{$menu_count}})</small></h4>
                             </div>
                         </a>
                     </div>
                     <div class="col">
                         <a class="card shadow border-0" href="/customers/">
                             <div class="card-body text-center">
-                                <h4 class="h2 text-primary text-uppercase"><i class="fas fa-users"></i> 客戶管理</h4>
+                                <h4 class="h2 text-primary text-uppercase"><i class="fas fa-users"></i> 客戶管理 <small>({{$customer_count}})</small></h4>
                             </div>
                         </a>
                     </div>
@@ -31,7 +31,7 @@
                     <div class="col">
                         <a class="card shadow border-0" href="/orders/">
                             <div class="card-body text-center">
-                                <h4 class="h2 text-primary text-uppercase"> <i class="fas fa-list"></i> 歷史訂單</h4>
+                                <h4 class="h2 text-primary text-uppercase"> <i class="fas fa-list"></i> 歷史訂單 <small>({{$ticket_count}})</small></h4>
                             </div>
                         </a>
                     </div>
@@ -47,10 +47,18 @@
             </div>
 
             <div class="col-lg-6">
-                <div class="card p-3 rounded shadow-sm mb-4">
-                    @if ($recently_tickets->count() == 0)
-                    <h3>前往<a class="text-primary" href="/customers/">客戶資料頁</a>，開始你新的交易吧!</h3>
+                @if ($recently_tickets->count() == 0)
+                <div class="card bg-gradient-primary text-white border-0 p-3">
+                    <h2 class="text-white text-center py-3"><i class="fa fa-bell text-yellow"></i> 歡迎使用，系統導引如下</h2>
+                    @if ($menu_count == 0)
+                    <h3 class="text-light py-2"><a class="text-white" href="/menus/">步驟 1: 營業項目:</a> 新增你的營業項目，讓系統幫你自動帶入設定吧</h3>
+                    @endif
+                    @if ($customer_count == 0)
+                    <h3 class="text-light py-2"><a class="text-white" href="/customers/">步驟 2: 客戶資料頁:</a> 建立你的會員資料庫</h3>
+                    @endif
+                    <h3 class="text-light py-2"><a class="text-white" href="/customers/">步驟 3: 客戶資料頁:</a> 開始建立訂單吧</h3>
                     @else
+                <div class="card p-3 rounded shadow-sm mb-4">
                     <h3 class="text-center">近期交易紀錄</h3>
                     <div>
                         @if ($recently_tickets)
