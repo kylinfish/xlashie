151c35b424d4d6d506c8a2d427677803ceda7858
kylinfish <kylinfish@gmail.com>
2021-04-02 15:55:05 +0800

    Add created_by and labeled_by to inventory for identifying multip user operations

--- api/app/Models/OpLog.php
+++ api/app/Models/OpLog.php
@@ -19,6 +19,20 @@ class OpLog extends Model
         "Api\Inventories" => 10,
     ];
 
+    const CONTROL_MAP_FOR_HUMAN = [
+        1 => "首頁",
+        2 => "公司",
+        3 => "客戶",
+        4 => "客戶",
+        5 => "營業項目",
+        6 => "營業項目",
+        7 => "訂單交易",
+        8 => "訂單交易",
+        9 => "庫存筆記",
+        10 => "客戶商品庫存",
+    ];
+
+
     const ACTION_MAP = [
         "index" => 0,
         "show" => 1,
@@ -31,6 +45,20 @@ class OpLog extends Model
         "search" => 8,
         "detail" => 9,
         "upload" => 10,
+    ];
+
+    const ACTION_MAP_FOR_HUMAN = [
+        0 => "瀏覽",
+        1 => "查看",
+        2 => "建立",
+        3 => "建立",
+        4 => "修改",
+        5 => "更新",
+        6 => "刪除",
+        7 => "刪除",
+        8 => "查詢",
+        9 => "查看細節",
+        10 => "上傳",
     ];
 
     protected $table = "op_logs";
@@ -48,4 +76,9 @@ class OpLog extends Model
     protected $attributes = [
         "sth" => ''
     ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
