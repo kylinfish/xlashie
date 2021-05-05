86cd4dc5417571f999a135d9d75ad6fd8338a484
kylinfish <kylinfish@gmail.com>
2021-05-05 15:36:21 +0800

    Menu 頁面顯示調整

--- api/resources/views/menus/index.blade.php
+++ api/resources/views/menus/index.blade.php
@@ -33,24 +33,24 @@
             <table class="table align-items-center table-sm table-flush">
                 <thead class="thead-light">
                     <tr>
-                        <th class="text-enter">名稱</th>
-                        <th class="text-enter"><i class="fa fa-dollar-sign" aria-hidden="true"></i> 售價</th>
-                        <th class="text-enter">預設狀態</th>
-                        <th class="text-enter">建立時間</th>
-                        <th class="text-enter">操作</th>
+                        <th class="text-center">名稱</th>
+                        <th class="text-center"><i class="fa fa-dollar-sign" aria-hidden="true"></i> 售價</th>
+                        <th class="text-center">預設狀態</th>
+                        <th class="text-center">建立時間</th>
+                        <th class="text-center">操作</th>
                     </tr>
                 </thead>
                 <tbody class="list">
                     @foreach ($menus as $menu)
                     <tr>
-                        <td>
+                        <td class="text-center">
                             <p class="font-weight-600 name mb-0">
                                 <a href="/menus/{{ $menu->id }}">{{ $menu->name }}</a>
                             </p>
                         </td>
-                        <td>{{ $menu->price }}</td>
-                        <td>{{ \App\Models\CustomerInventory::getStatusWording($menu->init_status) }}</td>
-                        <td>{{ $menu->created_at}}</td>
+                        <td class="text-center">{{ number_format($menu->price) }}</td>
+                        <td class="text-center">{{ \App\Models\CustomerInventory::getStatusWording($menu->init_status) }}</td>
+                        <td class="text-center">{{ $menu->created_at}}</td>
                         <td class="text-right form-inline">
                             <a href="/menus/{{ $menu->id }}" class="btn btn-outline-default">編輯</a>
                             <form action="/menus/{{ $menu->id }}" method="POST">
