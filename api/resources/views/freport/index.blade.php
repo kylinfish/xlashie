ec24d0e0352300134ea90b92b8e9560a60de6ec3
kylinfish <kylinfish@gmail.com>
2021-05-05 15:40:54 +0800

    新增營收報表

---
+++ api/resources/views/freport/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+@section('content')
+<div class="offset-lg-1 col-lg-10">
+    @include('common.alert')
+    <div class="row align-items-center mb-3">
+        <h1>營收報表 - 當月</h1>
+    </div>
+
+    @if (count($freports) == 0)
+    <div class="badge badge-warning">
+        <h3>從建立您的第一筆訂單開始吧</h3>
+    </div>
+    @else
+    <div class="card">
+    <div class="table-responsive">
+        <table class="table align-items-center table-flush table-striped">
+            <thead class="thead-light">
+                <tr>
+                    <th class="text-center">日期</th>
+                    <th class="text-center">營收總額</th>
+                </tr>
+            </thead>
+            <tbody class="">
+                @foreach ($freports as $date => $sums)
+                <tr>
+                    <td class="text-center">{{ $date }}</td>
+                    <td class="text-center">{{ number_format($sums) }}</td>
+                </tr>
+                @endforeach
+                <tr class="table-warning">
+                    <td class="text-center"><b>總計</b></td>
+                    <td class="text-center"><b>{{ number_format($total_income) }}</b></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</div>
+    @endif
+
+    @endsection
